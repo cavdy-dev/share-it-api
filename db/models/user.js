c830acf332b8684cf0b10625c8e48a2b62d38ca6
Cavdy <cavdyofficials@gmail.com>
2020-01-25 07:42:36 +0100

    setup database sequelize

---
+++ db/models/user.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        required: true
+      }
+    },
+    {}
+  );
+  User.associate = models => {
+    // associations can be defined here
+  };
+  return User;
+};
