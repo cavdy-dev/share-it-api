ff4110891f1c93138624f4e7d920d7118cc877a1
Cavdy <cavdyofficials@gmail.com>
2020-01-26 21:56:43 +0100

    login user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,9 +4,9 @@ import userValidation from '../middlewares/validations/userValidation';
 import headers from '../middlewares/headers';
 import check from '../middlewares/check';
 
-const { createUser } = userController;
+const { createUser, loginUser } = userController;
 const { generateToken, clientKey } = headers;
-const { checkIfEmailExist } = check;
+const { checkIfEmailExist, checkIfEmailDontExist } = check;
 
 const router = express.Router();
 
@@ -17,6 +17,14 @@ router.post(
   checkIfEmailExist,
   generateToken,
   createUser
+);
+router.post(
+  '/login',
+  clientKey,
+  userValidation,
+  checkIfEmailDontExist,
+  generateToken,
+  loginUser
 );
 
 export default router;
